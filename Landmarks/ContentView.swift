a1b15c72abe0cf6e51d6df18ce56acb90fc6abc3
Nathan Wong <nathan.wong2002@gmail.com>
2024-11-10 11:53:33 -0700

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Nathan Wong on 2024-11-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Nathan Wong on 2024-11-10.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
